49d1e4a23b35983f5395b369b92b235885bae462
Igwbriel <gabrieldias7820@gmail.com>
2024-03-28 21:03:32 -0300

    configurando eslint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello world!'
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP server running'); 
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running')
+  })
